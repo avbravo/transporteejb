987b7f41c7833152e00b24b11c97949a9c6479e7
avbravo <avbravo@avbravo>
2019-09-10 15:12:33 -0500

    VIAJES Pendientes

--- src/main/java/com/avbravo/transporteejb/services/RolServices.java
+++ src/main/java/com/avbravo/transporteejb/services/RolServices.java
@@ -2,7 +2,7 @@
 * To change this license header, choose License Headers in Project Properties.
 * To change this template file, choose Tools | Templates
  * and open the template in the editor.
-*/
+ */
 package com.avbravo.transporteejb.services;
 
 import com.avbravo.jmoordbutils.JsfUtil;
@@ -27,43 +27,44 @@ public class RolServices {
 
     @Inject
     RolRepository repository;
-    
-     @Inject
+
+    @Inject
     UsuarioRepository usuarioRepository;
-List<Rol> rolList = new ArrayList<>();
+    List<Rol> rolList = new ArrayList<>();
+
     public List<Rol> complete(String query) {
         List<Rol> suggestions = new ArrayList<>();
-             try {
-          suggestions=repository.complete(query);
+        try {
+            suggestions = repository.complete(query);
         } catch (Exception e) {
             JsfUtil.errorMessage("complete() " + e.getLocalizedMessage());
         }
 
-           return suggestions;
+        return suggestions;
     }
+
     public List<Rol> completeFiltrado(String query) {
         List<Rol> suggestions = new ArrayList<>();
-           try {
-               query = query.trim();
-               if (query.length() < 1) {
-                   return suggestions;
-               }   
-               String field = (String) UIComponent.getCurrentComponent(FacesContext.getCurrentInstance()).getAttributes().get("field");               
-               suggestions=  repository.findRegex(field,query,true,new Document(field,1));
+        try {
+            query = query.trim();
+            if (query.length() < 1) {
+                return suggestions;
+            }
+            String field = (String) UIComponent.getCurrentComponent(FacesContext.getCurrentInstance()).getAttributes().get("field");
+            suggestions = repository.findRegex(field, query, true, new Document(field, 1));
 
-           } catch (Exception e) {
-                    JsfUtil.errorMessage("complete() " + e.getLocalizedMessage());
-           }
-           return suggestions;
+        } catch (Exception e) {
+            JsfUtil.errorMessage("complete() " + e.getLocalizedMessage());
+        }
+        return suggestions;
     }
 
-    
     // <editor-fold defaultstate="collapsed" desc="getRolList()">
     public List<Rol> getRolList() {
         try {
-           rolList= repository.findAll(new Document("rol",1));
+            rolList = repository.findAll(new Document("rol", 1));
         } catch (Exception e) {
-              JsfUtil.errorMessage("getRolList() " + e.getLocalizedMessage());
+            JsfUtil.errorMessage("getRolList() " + e.getLocalizedMessage());
         }
         return rolList;
     }// </editor-fold>
@@ -71,40 +72,38 @@ List<Rol> rolList = new ArrayList<>();
     public void setRolList(List<Rol> rolList) {
         this.rolList = rolList;
     }
-    
-      // <editor-fold defaultstate="collapsed" desc="isDeleted(Rol rol)">
-  
-    public Boolean isDeleted(Rol rol){
-        Boolean found=false;
+
+    // <editor-fold defaultstate="collapsed" desc="isDeleted(Rol rol)">
+    public Boolean isDeleted(Rol rol) {
+        Boolean found = false;
         try {
-            Document doc = new Document("rol.idrol",rol.getIdrol());
+            Document doc = new Document("rol.idrol", rol.getIdrol());
             Integer count = usuarioRepository.count(doc);
-            if (count > 0){
+            if (count > 0) {
                 return false;
             }
-            
+
         } catch (Exception e) {
-             JsfUtil.errorMessage("isDeleted() " + e.getLocalizedMessage());
+            JsfUtil.errorMessage("isDeleted() " + e.getLocalizedMessage());
         }
         return true;
     }  // </editor-fold>
-    
-     // <editor-fold defaultstate="collapsed" desc="findById(String id)">
 
-    public Rol findById(String id){
-           Rol rol = new Rol();
+    // <editor-fold defaultstate="collapsed" desc="findById(String id)">
+    public Rol findById(String id) {
+        Rol rol = new Rol();
         try {
-         
+
             rol.setIdrol(id);
             Optional<Rol> optional = repository.findById(rol);
             if (optional.isPresent()) {
-               return optional.get();
-            } 
+                return optional.get();
+            }
         } catch (Exception e) {
-             JsfUtil.errorMessage("findById() " + e.getLocalizedMessage());
+            JsfUtil.errorMessage("findById() " + e.getLocalizedMessage());
         }
-      
-      return rol;
+
+        return rol;
     }
     // </editor-fold>
 }
